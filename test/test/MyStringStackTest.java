4bd8202824d762f0b571541b55e8e902345f626f
pngqj <37372521+pngqj@users.noreply.github.com>
2022-07-19 11:22:15 +0800

    whatever

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -31,7 +31,7 @@ public class MyStringStackTest extends TestCase {
 
 	@Test
 	public void testPush() {
-// 		String o1 = null;  
+	String o1 = null;  
 // 		MyStringStack stack = new MyStringStack();
 // 		assertTrue (stack.isEmpty());
 // 		stack.push (s1);
